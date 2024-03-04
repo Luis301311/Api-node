e37e2da7ecf084910fd285cbbf9eea83526a01c5
Luis301311 <luisalbertovega@unicesar.edu.co>
2024-03-04 00:49:59 -0500

    puerto

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 17513;
+const PORT = process.env.PORT || 3000;
 const dbHost = process.env.DB_HOST || 'monorail.proxy.rlwy.net';
 const dbUser = process.env.DB_USER || 'root';
 const dbPassword = process.env.DB_PASSWORD || 'd-f12b421D5fDH26cCDbgh5bGF1G3H5H';
 const dbName = process.env.DB_NAME || 'railway';
-const dbPort = process.env.DB_PORT || 3606;
+const dbPort = process.env.DB_PORT || 17513;
 
 module.exports = {
     PORT: PORT,
