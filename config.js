6580d9606200c4d0fdc8858ba5b70fb70a4c2f5c
Luis301311 <luisalbertovega@unicesar.edu.co>
2024-03-04 01:53:03 -0500

    commit

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const PORT = process.env.PORT || 3000;
-const dbHost = process.env.DB_HOST || 'monorail.proxy.rlwy.net';
-const dbUser = process.env.DB_USER || 'root';
-const dbPassword = process.env.DB_PASSWORD || 'd-f12b421D5fDH26cCDbgh5bGF1G3H5H';
-const dbName = process.env.DB_NAME || 'railway';
-const dbPort = process.env.DB_PORT || 17513;
+const dbHost = process.env.DB_HOST || 'bg6ixcnj8ifmxc0igvcr-mysql.services.clever-cloud.com';
+const dbUser = process.env.DB_USER || 'uog1u20zdsuoij9c';
+const dbPassword = process.env.DB_PASSWORD || 'tOWKlyt5cqkIGsk8kxMS';
+const dbName = process.env.DB_NAME || 'bg6ixcnj8ifmxc0igvcr';
+const dbPort = process.env.DB_PORT || 3306;
 
 module.exports = {
     PORT: PORT,
